6d9e675b63e395237a11c996804e25101f0ed594
RohithS05 <rohithkumar.sridharan@mail.utoronto.ca>
2024-06-02 22:38:32 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,54 @@
-package tt1.java;
+import java.util.*;
+
+import java.io.*;
 
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial(); 
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2); 
-        Polynomial s = p1.add(p2); 
-        System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+  public static void main(String[] args) {
+    double[] c1 = {1, 1};
+    int[] e1 = {1, 0};
+    Polynomial p1 = new Polynomial(c1, e1);
+    double[] c11 = {-1, -1};
+    int[] e11 = {1, 0};
+    Polynomial p11 = new Polynomial(c11, e11);
+    double[] c2 = {1, 2};
+    int[] e2 = {1, 0};
+    Polynomial p2 = new Polynomial(c2, e2);
+
+    Polynomial s = p1.multiply(p2);
+    Polynomial d = new Polynomial();
+    Polynomial zeroSum = p1.add(p11);
+    System.out.println(Arrays.toString(zeroSum.coefficients));
+    System.out.println(Arrays.toString(zeroSum.exponents));
+    if (Polynomial.isZeroPolynomial(zeroSum)) System.out.println("Zero sum works");
+
+    try {
+      s.saveToFile("abc1.txt");
+      p1.saveToFile("abc2.txt");
+      p2.saveToFile("../polynomial.txt");
+      d.saveToFile("../dubey.txt");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     }
+
+    double a = s.evaluate(2);
+    System.out.println(a);
+
+    File file = new File("poly.txt");
+    File file2 = new File("empty.txt");
+    try {
+      Polynomial p3 = new Polynomial(file);
+      //System.out.println("Exponents: " + Arrays.toString(p3.exponents));
+      //System.out.println("Coeff: " + Arrays.toString(p3.coefficients));
+
+      Polynomial empty = new Polynomial(file2);
+      if (Polynomial.isZeroPolynomial(empty)) System.out.println("zero polynomial is working");
+      if (Polynomial.isZeroPolynomial(empty.add(empty))) System.out.println("zero polynomial is working");
+      if (Polynomial.isZeroPolynomial(empty.multiply(p3))) System.out.println("zero polynomial is working");
+      if (empty.hasRoot(15) == true) System.out.println("zero polynomial is working");
+      if (empty.evaluate(15) == 0) System.out.println("zero polynomial is working");
+    } catch (Exception e) {
+      System.out.println(e.getMessage() + "dubey");
+    }
+
+  }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,63 +1,251 @@
-package tt1.java;
-
 import java.lang.Math;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 class Polynomial {
-    double [] array;
+    double [] co_array;
+    int [] exp_array;
 
     public Polynomial(){
-        this.array = new double [1];
-        array[0] = 0;
+	this.co_array = new double [1];
+	co_array[0] = 0;
+	this.exp_array = new int [1];
+	exp_array[0] = 0;
     }
-    public Polynomial(double[] array){
-        this.array = new double [array.length];
-        for (int i = 0; i < array.length; i++){
-            this.array[i] = array[i];
-        }
+
+    public Polynomial(double[] co_array, int[] exp_array){
+	this.co_array = new double [co_array.length];
+	this.exp_array = new int [exp_array.length];
+	for (int i = 0; i < co_array.length; i++){
+	    this.co_array[i] = co_array[i];
+	    this.exp_array[i] = exp_array[i];
+	}
+    }
+
+    public Polynomial(int[] exp_array, double[] co_array){
+	this.co_array = new double [co_array.length];
+	this.exp_array = new int [exp_array.length];
+	for (int i = 0; i < co_array.length; i++){
+	    this.co_array[i] = co_array[i];
+	    this.exp_array[i] = exp_array[i];
+	}
+    }
+
+    public Polynomial(File file) throws FileNotFoundException, IOException {
+	BufferedReader input = new BufferedReader(new FileReader(file.getAbsolutePath()));
+	String line = input.readLine();
+	input.close();
+
+	if (line != null){
+	    parse_poly(line);
+	}
+    }
+
+    public boolean inside(char val, String string){
+	for (int i = 0; i < string.length(); i++){
+	    char c = string.charAt(i);
+	    if (c == val){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public void parse_poly(String string){
+	String[] terms = string.split("(?=\\+)|(?=-)");
+	double [] co_array = new double [terms.length];
+	int [] exp_array = new int [terms.length];
+
+
+	for (int i = 0; i < terms.length; i++){
+	    if (inside('x', terms[i]) == false){
+		co_array[i] = Double.parseDouble(terms[i]);
+		exp_array[i] = 0;
+		continue;
+	    }
+	    String[] temp_term = terms[i].split("x");
+	    co_array[i] = Double.parseDouble(temp_term[0]);
+	    exp_array[i] = Integer.parseInt(temp_term[1]);
+	}
+
+	this.co_array = new double [co_array.length];
+	this.exp_array = new int [exp_array.length];
+	for (int i = 0; i < co_array.length; i++){
+	    this.co_array[i] = co_array[i];
+	    this.exp_array[i] = exp_array[i];
+	}
+    }
+
+    public boolean inside(int val, int [] array){
+	for (int i = 0; i < array.length; i++){
+	    if (val == array[i])
+		return true;
+	}
+	return false;
+    }
+
+    public Polynomial remove_zero(double [] co_array, int[] exp_array){
+	int length = exp_array.length;
+
+	int [] new_exp_array = new int [length];
+	double [] new_co_array = new double [length];
+
+	int j = 0;
+
+	for (int i = 0; i < length; i++){
+	    if (co_array[i] != 0){
+		new_co_array[j] = co_array[i];
+		new_exp_array[j] = exp_array[i];
+		j++;
+	    }
+	}
+
+	if (j == 0){
+	    Polynomial return_poly = new Polynomial();
+	    return return_poly;
+	}
+
+	Polynomial return_poly = new Polynomial(new_co_array, new_exp_array);
+	return return_poly;
     }
 
     public Polynomial add(Polynomial polynomial){
-        int length = Math.max(array.length, (polynomial.array).length);
-        double [] new_array = new double [length];
-        for (int i = 0; i < length; i++){
-        	if (i >= array.length) {
-        		new_array[i] = polynomial.array[i];
-        	}
-        	else if (i >= polynomial.array.length) {
-        		new_array[i] = array[i];
-        	}
-        	else {
-        		new_array[i] = array[i] + polynomial.array[i];
-        	}
-        }
-        Polynomial return_poly = new Polynomial(new_array);
-        return return_poly; 
+
+	int length = Math.max(co_array.length, (polynomial.co_array).length);
+	double [] new_co_array = new double [length];
+	int [] new_exp_array = new int [length];
+
+	int j = 0;
+	int i = 0;
+	boolean flag = false;
+
+	while (i < length){
+	    flag = false;
+
+	    int exp;
+	    if (i < exp_array.length)
+		exp = exp_array[i];
+	    else {
+		exp = (polynomial.exp_array)[i];
+	    }
+
+	    if (inside(exp, new_exp_array))
+		continue;
+	    else
+		new_exp_array[j]=exp;
+		flag = true;
+
+	    if (inside(exp, exp_array) && inside(exp, polynomial.exp_array)){
+		if (i >= co_array.length) {
+		    new_co_array[j] = polynomial.co_array[i];
+		}
+		else if (i >= (polynomial.co_array).length) {
+		    new_co_array[j] = co_array[i];
+		}
+		else {
+		    new_co_array[j] = co_array[i] + polynomial.co_array[i];
+		}
+	    }
+
+	    else  if (inside(exp, exp_array)){
+		new_co_array[j] = co_array[i];
+	    }
+
+	    else {
+		new_co_array[j] = polynomial.co_array[i];
+	    }
+
+	    i = i+1;
+	    if (flag)
+		j = j+1;
+
+	}
+
+	Polynomial return_poly = remove_zero(new_co_array, new_exp_array);
+	return return_poly;
     }
 
     public double evaluate(double x){
-        double counter = array[0];
-        double val = 1;
-        for (int i = 1; i < array.length; i++){
-        	for (int j = 0; j < i; j++) {
-        		val *= x;
-        	}
-            counter += array[i]*val;
-            val = 1;
-        }
-        return counter;
-    }
-    
-	public double evaluate(int x) {
-        double y = x;
-        return evaluate(y);
+	int length = co_array.length;
+	double counter = 0;
+
+	for (int i = 0; i < length; i++){
+	    int exp = exp_array[i];
+	    double co = co_array[i];
+
+	    counter += co*(Math.pow(x, exp));
 	}
 
-    public boolean hasRoot(double val){
-        if (this.evaluate(val) == 0){
-            return true;
-        }
-        return false;
+	return counter;
     }
 
+    public boolean hasRoot(double val){
+	if (this.evaluate(val) == 0){
+	    return true;
+	}
+	return false;
+    }
+
+    public Polynomial multiply(Polynomial polynomial){
+	int length = Math.max(co_array.length, (polynomial.co_array).length);
+	double [] new_co_array = new double [length];
+	int [] new_exp_array = new int [length];
+	Polynomial return_poly = new Polynomial(new_co_array, new_exp_array);
+
+	for (int i = 0; i < co_array.length; i++){
+	    int exp = exp_array[i];
+	    double val = co_array[i];
+
+	    double [] temp_co_array = new double [length];
+	    int [] temp_exp_array = new int [length];
+
+	    for (int j = 0; j < (polynomial.co_array).length; j++){
+		temp_co_array[j] = val*(polynomial.co_array[j]);
+		temp_exp_array[j] = exp+(polynomial.exp_array[j]);
+
+	    }
+
+	    Polynomial temp_poly = remove_zero(temp_co_array, temp_exp_array);
+	    return_poly = new Polynomial(new_co_array, new_exp_array);
+	    return_poly = return_poly.add(temp_poly);
+	}
+
+	return return_poly;
+    }
+
+    public void saveToFile(String file_name){
+	String temp_poly = "";
+	int length = co_array.length;
+	for (int i = 0; i < length; i++){
+	    if (exp_array[i] == 0){
+		if (co_array[i] > 0){
+		    temp_poly += "+";
+		}
+		temp_poly += Double.toString(co_array[i]);
+		continue;
+	    }
+
+	    if (co_array[i] > 0){
+		temp_poly += "+";
+	    }
+	    temp_poly += Double.toString(co_array[i]);
+	    temp_poly += "x";
+	    temp_poly += Integer.toString(exp_array[i]);
+	}
+	if (temp_poly.charAt(0) == '+'){
+	    temp_poly = temp_poly.substring(1);
+	}
+
+	try {
+	    FileWriter myWriter = new FileWriter(file_name);
+	    myWriter.write(temp_poly);
+	    myWriter.close();
+	} catch (IOException e){
+	}
+    }
 
 }
